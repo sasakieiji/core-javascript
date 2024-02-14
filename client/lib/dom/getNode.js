162e15e4774732ad4c9b8ed0450c12aad7ad0652
TadakTadak <wltjd9711@gmail.com>
2024-02-14 10:31:04 +0900

    주접 생성기 기본 설정

--- client/lib/dom/getNode.js
+++ client/lib/dom/getNode.js
@@ -1,5 +1,5 @@
 
-function getNode(node,context = document){
+export function getNode(node,context = document){
 
   if(typeof node !== 'string'){
     throw new Error('getNode 함수의 인수는 문자 타입 이어야 합니다.');
@@ -13,7 +13,7 @@ function getNode(node,context = document){
 
 
 
-function getNodes(node,context = document){
+export function getNodes(node,context = document){
   if(typeof node !== 'string'){
     throw new Error('getNodes 함수의 인수는 문자 타입 이어야 합니다.');
   }
--- client/lib/dom/insert.js
+++ client/lib/dom/insert.js
@@ -1,24 +1,28 @@
+import { getNode } from "./getNode.js";
 
-function insertBefore(node,text){
+export function insertBefore(node,text){
   if(typeof node === 'string') node = getNode(node);
 
   node.insertAdjacentHTML('beforebegin',text)
 }
 
-function insertFirst(node,text){
+export function insertFirst(node,text){
   if(typeof node === 'string') node = getNode(node);
 
   node.insertAdjacentHTML('afterbegin',text)
 }
 
-function insertLast(node,text){
+export function insertLast(node,text){
   if(typeof node === 'string') node = getNode(node);
 
   node.insertAdjacentHTML('beforeend',text)
 }
 
-function insertAfter(node,text){
+export function insertAfter(node,text){
   if(typeof node === 'string') node = getNode(node);
 
   node.insertAdjacentHTML('afterend',text)
 }
+
+
+
